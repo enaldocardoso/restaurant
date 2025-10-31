c642e54cccbdf7c4ff95512f423ff4841913adc0
Enaldo Cardoso Junior <enaldocardoso@gmail.com>
2025-10-31 11:00:43 -0300

    feat: finished fisrt screen

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -2,6 +2,7 @@ import { getRestaurantBySlug } from "@/data/get-restaurant-by-slug";
 import { db } from "@/lib/prisma";
 import Image from "next/image";
 import { notFound } from "next/navigation";
+import ConsumptionMethodOption from "./components/consumption-method-option";
 
 interface RestaurantPageProps {
   params: Promise<{ slug: string }>;
@@ -17,6 +18,7 @@ const RestaurantPage = async ({ params }: RestaurantPageProps) => {
   }
   return (
     <div className="flex h-screen flex-col items-center justify-center px-6 pt-24">
+      {/*logo e titulo*/}
       <div className="flex flex-col items-center gap-2">
         <Image
           src={restaurant.avatarImageUrl}
@@ -26,12 +28,29 @@ const RestaurantPage = async ({ params }: RestaurantPageProps) => {
         />
         <h2 className="font-semibold">{restaurant.name}</h2>
       </div>
+      {/*bem vindo*/}
       <div className="space-y-2 pt-24 text-center">
         <h3 className="text-2xl font-semibold">Seja bem-vindo!</h3>
         <p className="opacity-55">
           Escolha como prefere aproveitar sua refeição. Estamos oferecer
           paraticidade e sabor em cada detalhe!
         </p>
+      </div>
+      <div className="grid grid-cols-2 gap-4 pt-14">
+        <ConsumptionMethodOption
+          slug={slug}
+          option="DINE_IN"
+          buttonText="Para comer aqui"
+          imageAlt="Comer aqui"
+          imageUrl="/dine_in.png"
+        />
+        <ConsumptionMethodOption
+          slug={slug}
+          option="TAKEAWAY"
+          buttonText="Para levar"
+          imageAlt="Comer levar"
+          imageUrl="/takeaway.png"
+        />
       </div>
     </div>
   );
